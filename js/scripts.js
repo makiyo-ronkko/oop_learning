519f066bcc63ba86fbcbf2516ab1252f1b6772db
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-03-19 12:28:03 +0200

    original bird removed

--- js/scripts.js
+++ js/scripts.js
@@ -10,23 +10,34 @@ $(document).ready(function () {
         return { // return to objects addBird:function & init:function as properties
 
             birdId: null,//create a new property and empty until id given
-            birdSize: null,
+            birdSize: null, // integer
+            movementRatio: null, // to be divided from scrolling pixels
+            topOffset: null,
             addBird: function () {
                 var _newBird = document.createElement('img');
 
                 _newBird.src = 'assets/bird.png';
                 _newBird.className = 'bird';
                 _newBird.id = this.birdId;
-                _newBird.style.width = this.birdSize;
 
-                _newBird.style.left = this.birdSize;
+                _newBird.style.width = this.birdSize + 'px';
+                _newBird.style.left = this.birdSize + 'px';
+                _newBird.style.top = this.topOffset + '%';
+                _newBird.style.filter = 'hue-rotate(' + getRandomInt(0, 270) + 'deg)';
 
                 // _newBird.id = 'bird'; //styling with #bird
                 document.body.appendChild(_newBird);
             },
+
+            fly: function (scrollDistance) {
+                document.getElementById(this.birdId).style.left = this.birdSize + scrollDistance / this.movementRatio + 'px';
+            },
+
             init: function () {// need to create document.body.appendChild(_newBird);
-                this.birdId = 'bird_' + getRandomInt(100, 999);
-                this.birdSize = getRandomInt(50, 500) + 'px';
+                this.topOffset = getRandomInt(10, 70);
+                this.movementRatio = getRandomInt(10, 50);
+                this.birdId = 'bird_' + getRandomInt(0, 12345);
+                this.birdSize = getRandomInt(0, 500) + 'px';
                 this.addBird();
             }
         };
@@ -44,18 +55,22 @@ $(document).ready(function () {
 
     //$('#pipe').css('top, $(window).height()-$('#pipe').height() + 'px');
     $(window).scroll(function (event) {
-        var offset = $(window).scrollTop();
+        var offset = $(window).scrollTop(); //how far from the right of the browser
 
         // $(window).scrollTop gives you the distance to the top of the page
         var imgX = offset / 50;
         var pipeX = offset / 20;
-        var birdX = offset / 5;
+        var birdX = offset / 10;
 
         console.log(imgX);
         // move these elements sideway
         $('#background').css('background-position', imgX + 'px 0px');
-        $('#bird').css('left', pipeX);
-        $('#pipe').css('left', birdX);
+        //$('#bird').css('left', birdX);
+        $('#pipe').css('left', pipeX);
+        bird1.fly(offset);
+        bird2.fly(offset);
+        bird3.fly(offset);
+        bird4.fly(offset);
 
         $(document).on('click', flyUp);
 
